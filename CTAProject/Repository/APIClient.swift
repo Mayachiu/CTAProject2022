4c774d073a29f8aff666384fa131b917d8526eca
Mayachiu <mayachiu5g@gmail.com>
2022-01-20 14:09:28 +0900

    Fix APIKey property name

--- CTAProject/Repository/APIClient.swift
+++ CTAProject/Repository/APIClient.swift
@@ -12,7 +12,7 @@ enum APIClient {
         let encodedSearchWord = searchWord.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
         
         //別ファイルからAPIキー読み込み
-        let APIKey = MyAPIKey.hotpepperAPIKey
+        let APIKey = MyAPIKey.hotpepper
         guard let url = URL(string: "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=" + APIKey + "&format=json&keyword=" + String(encodedSearchWord!)) else { return completion(.failure(.textEncodingError)) }
         
         var request = URLRequest(url: url)
