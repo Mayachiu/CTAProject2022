6bf44c859b095cce8036bdb0ddfff7a60dbd0344
Mayachiu <mayachiu5g@gmail.com>
2022-01-24 12:47:11 +0900

    Fix Extention and nibName and Identifier

---
+++ CTAProject/Extension/UITableViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Extension.swift
+//  CTAProject
+//
+//  Created by 内山和輝 on 2022/01/24.
+//
+
+import Foundation
+import UIKit
+
+extension UITableViewCell {
+    static let shopTableViewCellNibName = String(describing: ShopTableViewCell.self) //クラス名が取得できる
+    static let shopTableViewCellIdentifier = "Cell"
+}
