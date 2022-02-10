93aed4740884301c37a3b58b8e6af2bc257dc466
mayachiu <mayachiu5g@gmail.com>
2022-02-10 23:50:13 +0900

    Update AlertView.swift

--- CTAProject/UIComponent/AlertView.swift
+++ CTAProject/UIComponent/AlertView.swift
@@ -10,13 +10,7 @@ import UIKit
 final class AlertView: UIView {
     @IBOutlet private weak var alertLabel: UILabel!
     @IBOutlet private weak var closeButton: UIButton!
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+
     override func draw(_ rect: CGRect) {
         alertLabel.text = L10n.characterAlertText
         closeButton.setTitle(L10n.closeButtonTitle, for: .normal)
